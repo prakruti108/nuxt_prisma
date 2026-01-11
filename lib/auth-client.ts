ae2a951bf54cecae719920befc9b001d9d35efd2
KST DEV <dev@konsoftech.in>
2026-01-11 14:06:32 +0100

    hardcoded auth client keys

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,8 +1,7 @@
 import { createAuthClient } from "better-auth/vue"
-
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: useRuntimeConfig().public.BETTER_AUTH_URL || 'http://auth.konsoftech.in:3000',
+    baseURL: process.env.BETTER_AUTH_URL || 'https://nuxt-prisma-gltp.vercel.app/',
     /** The secret key used to sign tokens (required) */
-    secret: useRuntimeConfig().public.BETTER_AUTH_SECRET || 'somesecretkey',
+    secret: process.env.BETTER_AUTH_SECRET || 'GrnXHfBsH4mNGdJwhJqy5ktWDTIaiVCV',
 })
