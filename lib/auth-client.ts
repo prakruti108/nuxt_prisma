763eff30c617c4ce21b036191f5d8f6ad37420fc
KST DEV <dev@konsoftech.in>
2026-01-11 14:31:43 +0100

    generalised

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/vue"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: process.env.BETTER_AUTH_URL || 'https://nuxt-prisma-gltp.vercel.app/',
+    baseURL: useRuntimeConfig().public.betterAuthUrl || 'https://nuxt-prisma-gltp.vercel.app/',
     /** The secret key used to sign tokens (required) */
-    secret: process.env.BETTER_AUTH_SECRET || 'GrnXHfBsH4mNGdJwhJqy5ktWDTIaiVCV',
+    secret: useRuntimeConfig().public.betterAuthSecret || 'GrnXHfBsH4mNGdJwhJqy5ktWDTIaiVCV',
 })
