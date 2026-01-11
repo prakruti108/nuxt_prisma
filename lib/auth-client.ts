4e23cc0d722b3dc916b76c134e3111f63f9c4c2b
KST DEV <dev@konsoftech.in>
2026-01-11 14:02:03 +0100

    retry config

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,8 @@
 import { createAuthClient } from "better-auth/vue"
+
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: process.env.BETTER_AUTH_URL || 'http://auth.konsoftech.in:3000',
+    baseURL: useRuntimeConfig().public.BETTER_AUTH_URL || 'http://auth.konsoftech.in:3000',
     /** The secret key used to sign tokens (required) */
-    secret: process.env.BETTER_AUTH_SECRET || 'somesecretkey',
+    secret: useRuntimeConfig().public.BETTER_AUTH_SECRET || 'somesecretkey',
 })
