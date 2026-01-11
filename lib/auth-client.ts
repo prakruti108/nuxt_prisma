807e90effa1508e2922359ca1ff1d6cadbaa10e1
KST DEV <dev@konsoftech.in>
2026-01-11 13:53:22 +0100

    fix for config

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,9 +1,7 @@
 import { createAuthClient } from "better-auth/vue"
-import { useRuntimeConfig } from '#app'
-const config = useRuntimeConfig()
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: (config.public.betterAuthUrl as string) || 'http://dev.konsoftech.in:3000',
+    baseURL: process.env.BETTER_AUTH_URL || 'http://auth.konsoftech.in:3000',
     /** The secret key used to sign tokens (required) */
-    secret: (config.public.betterAuthSecret as string) || 'somesecretkey',
+    secret: process.env.BETTER_AUTH_SECRET || 'somesecretkey',
 })
