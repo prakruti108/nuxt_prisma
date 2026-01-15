0bd2eed8ef72c34bdc60eb5ff3b3a0dfa70818d1
KST DEV <dev@konsoftech.in>
2026-01-15 16:26:14 +0100

    finally working after hardcodings removed

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,3 @@
 import { createAuthClient } from "better-auth/vue"
 export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: useRuntimeConfig().public.betterAuthUrl || '/',
-    /** The secret key used to sign tokens (required) */
-    secret: useRuntimeConfig().public.betterAuthSecret || '',
 })
